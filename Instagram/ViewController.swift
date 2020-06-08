ef38a63c17a408540a0254a98898b5f0b5ccbec6
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-08 21:49:44 +0530

    Code cleared

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -1,20 +1,9 @@
-//
-//  ViewController.swift
-//  Instagram
-//
-//  Created by Hansa Anuradha on 6/8/20.
-//  Copyright © 2020 Hansa Anuradha. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
