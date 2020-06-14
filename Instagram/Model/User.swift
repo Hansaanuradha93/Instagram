618e0ec82c97d85dccffadd08b6178c7e18e8b59
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-14 21:43:34 +0530

    User model changed

--- Instagram/Model/User.swift
+++ Instagram/Model/User.swift
@@ -5,13 +5,15 @@ struct User {
     
     // MARK: Properties
     let uid: String
+    let email: String
     let username: String
     let profileImageUrl: String
     
     
     // MARK: Initializers
-    init(uid: String, username: String, profileImageUrl: String) {
+    init(uid: String, email: String, username: String, profileImageUrl: String) {
         self.uid = uid
+        self.email = email
         self.username = username
         self.profileImageUrl = profileImageUrl
     }
@@ -20,8 +22,9 @@ struct User {
     init(snapshot: DataSnapshot) {
         let userDict = snapshot.value as? [String : AnyObject] ?? [:]
         let uid = Auth.auth().currentUser?.uid ?? ""
+        let email = (userDict["email"] as? String) ?? ""
         let username = (userDict["username"] as? String) ?? ""
         let profileImageUrl = (userDict["imageUrl"] as? String) ?? ""
-        self.init(uid: uid, username: username, profileImageUrl: profileImageUrl)
+        self.init(uid: uid, email: email, username: username, profileImageUrl: profileImageUrl)
     }
 }
