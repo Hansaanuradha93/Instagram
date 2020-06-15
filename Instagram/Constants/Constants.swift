0ab1d0c27cf8dcdbf683a0b19a40dcb299619579
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-15 22:08:33 +0530

    New constants added

--- Instagram/Constants/Constants.swift
+++ Instagram/Constants/Constants.swift
@@ -2,14 +2,25 @@ import UIKit
 
 enum Asserts {
     
-    static let homeSelected = UIImage(named: "home_selected")
-    static let homeUnselected = UIImage(named: "home_unselected")
-    static let profileSelected = UIImage(named: "profile_selected")
-    static let profileUnselected = UIImage(named: "profile_unselected")
+    // MARK: Tab Bar
+    static let homeSelected = UIImage(named: "home_selected")!
+    static let homeUnselected = UIImage(named: "home_unselected")!
+    static let profileSelected = UIImage(named: "profile_selected")!
+    static let profileUnselected = UIImage(named: "profile_unselected")!
 
+    // MARK: Login Scene
+    static let plusPhoto = UIImage(named: "plus_photo")!
 }
 
 
 enum Strings {
+    
+    // MARK: Common
     static let empty = ""
+    
+    // MARK: Login Scene
+    static let signUp = "Sign Up"
+    static let email = "Email"
+    static let username = "Username"
+    static let password = "Password"
 }
