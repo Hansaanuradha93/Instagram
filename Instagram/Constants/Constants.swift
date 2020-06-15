ca5b47726509cf8480bceb3663eaf2195aee89ac
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-06-15 21:56:47 +0530

    Asserts constants added

---
+++ Instagram/Constants/Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+enum Asserts {
+    
+    static let homeSelected = UIImage(named: "home_selected")
+    static let homeUnselected = UIImage(named: "home_unselected")
+    static let profileSelected = UIImage(named: "profile_selected")
+    static let profileUnselected = UIImage(named: "profile_unselected")
+
+}
